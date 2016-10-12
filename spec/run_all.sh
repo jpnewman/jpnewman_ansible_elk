413f9eb663040768031057676f442d320b83886b
jpnewman <johnpaul.newman@gmail.com>
2016-10-12 02:35:53 +0100

    Updated testinfra ssh config path.

--- spec/run_all.sh
+++ spec/run_all.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 echo "elk-server"
-testinfra -v test_elk-server.py --ssh-config=$HOME/.ssh/config --sudo --hosts=elk-server
+testinfra -v test_elk-server.py --ssh-config=.ssh_config --sudo --hosts=elk-server
 
 echo "elk-log-001"
-testinfra -v test_elk-log.py --ssh-config=$HOME/.ssh/config --sudo --hosts=elk-log-001
+testinfra -v test_elk-log.py --ssh-config=.ssh_config --sudo --hosts=elk-log-001
 
 echo "Artifactory"
-testinfra -n 2 -v test_artifactory.py --ssh-config=$HOME/.ssh/config --sudo --hosts=art01,art02
+testinfra -n 2 -v test_artifactory.py --ssh-config=.ssh_config --sudo --hosts=art01,art02
 
 echo "Jenkins"
-testinfra -v test_jenkins.py --ssh-config=$HOME/.ssh/config --sudo --hosts=jenkins-server
+testinfra -v test_jenkins.py --ssh-config=.ssh_config --sudo --hosts=jenkins-server
 
 echo "Gerrit"
-testinfra -v test_gerrit.py --ssh-config=$HOME/.ssh/config --sudo --hosts=gerrit-server
+testinfra -v test_gerrit.py --ssh-config=.ssh_config --sudo --hosts=gerrit-server
